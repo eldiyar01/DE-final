4dbd73931ff2207da3b4d61767de42676fb4e6a5
eldiyar <49130964+eldiyar01@users.noreply.github.com>
2022-06-03 13:15:31 +0600

    Add files via upload

---
+++ motion2.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+import pygame
+import random
+from pygame.locals import *
+pygame.init()
+GPIO.setmode(GPIO.BCM)
+PIR_PIN = 21
+GPIO.setup(PIR_PIN, GPIO.IN)
+phrases = list()
+phrases.append("hello.ogg")
+phrases.append("rus.ogg")
+phrases.append("kenobi.ogg")
+phrases.append("lina.ogg")
+phrase = pygame.mixer.Sound(random.choice(phrases))
+def MOTION(PIR_PIN):
+    pygame.mixer.Sound(random.choice(phrases)).play()
+
+try:
+    GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)#,bouncetime=2000 
+    while True:
+        time.sleep(10)
+
+except KeyboardInterrupt:
+    print("Quit")
+    GPIO.cleanup()
