542d75af9caf34b83416e96ced027e89435f4d64
eldiyar <49130964+eldiyar01@users.noreply.github.com>
2022-06-04 18:32:08 +0600

    Update motion2.py

--- motion2.py
+++ motion2.py
@@ -2,21 +2,24 @@ import RPi.GPIO as GPIO
 import time
 import pygame
 import random
+import os
 from pygame.locals import *
 pygame.init()
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 21
 GPIO.setup(PIR_PIN, GPIO.IN)
 phrases = list()
-phrases.append("hello.ogg")
-phrases.append("rus.ogg")
-phrases.append("kenobi.ogg")
-phrases.append("lina.ogg")
+#for sounds in repo
+#for file_n in os.listdir("sounds"):
+#    with open(os.path.join("sounds", file_n),"r") as f:
+#        phrases.append(f.name)
+#        print(phrases)
+phrases.append('rick.ogg')
+
 def MOTION(PIR_PIN):
     pygame.mixer.Sound(random.choice(phrases)).play()
-
 try:
-    GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)#,bouncetime=2000 
+    GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
     while True:
         time.sleep(10)
 
